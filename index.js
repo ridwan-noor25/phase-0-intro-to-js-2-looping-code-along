15125a5e9d3c6f7fe4d6d2df32a2b845cd3bb5da
ridwan-noor25 <ridh2562@gmail.com>
2025-03-08 11:27:04 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+for (let age = 30; age<40; age++) {
+    console.log(`i'm ${age} years old. Happy birthday to me!`);
+    debugger;
+}
+
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
